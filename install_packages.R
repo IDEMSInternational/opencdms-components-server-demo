12186cbb6db760e5c5fdefa5c2f541870426a8c1
Chris Clarke <chris.m.clarke@live.co.uk>
2022-03-25 09:30:54 -0700

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,4 +1,5 @@
 install.packages('devtools',repos = "http://cran.us.r-project.org")
 install.packages("rlang",repos = "http://cran.us.r-project.org")
-devtools::install_github("IDEMSInternational/cdms.products")
+# Note - the products library is not currently versionined so manually update head sha ref to install version
+devtools::install_github("IDEMSInternational/cdms.products",ref="2d4babe132927b27e1ac311d0a4693f702d68578")
 q()
