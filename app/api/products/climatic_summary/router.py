307c92879f4772cae6ddb98c1eaa8e7c1f0f2aba
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-26 23:25:20 -0700

    feat: add error handlers for summary and inventory plot

--- app/api/products/climatic_summary/router.py
+++ app/api/products/climatic_summary/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from app.api.products.climatic_summary.schema import ClimaticSummaryParams
 from sqlalchemy.orm.session import Session
 from app.api.products.schema import DataFrameResponseType, ProductDataParams
 from app.db import get_session
-from app.services.climatic_summary import FailedCreatingClimaticSummary, climatic_summary_create
+from app.services.climatic_summary import climatic_summary_create
 from app.utils.product_data import generateProductData
 from app.utils.response import get_error_response, get_success_response, get_dataframe_response
 
@@ -18,14 +19,16 @@ def create(
     response_type: DataFrameResponseType = DataFrameResponseType.records,
     db_session: Session = Depends(get_session),
 ):
-    data = generateProductData(data_params, db_session)
-
     try:
+        data = generateProductData(data_params, db_session)
+        if data.empty:
+            return JSONResponse(content=get_error_response(message="Error - No data exists for query"), status_code=404)
         df = climatic_summary_create(data, product_params)
         response = get_dataframe_response(dataframe=df, option=response_type)
         return get_success_response(
             result=response,
             message="Successfully created observation_final.",
         )
-    except FailedCreatingClimaticSummary as e:
-        return get_error_response(message=str(e))
+    # exception handlers
+    except Exception as e:
+        return JSONResponse(content=get_error_response(message=str(e)), status_code=400)
--- app/api/products/inventory_plot/router.py
+++ app/api/products/inventory_plot/router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from sqlalchemy.orm.session import Session
 from app.api.products.schema import FileResponseType, ProductDataParams
 from app.api.products.inventory_plot.schema import InventoryPlotParams
 from app.db import get_session
 from app.services.inventory_plot import inventory_plot_create
 from app.utils.product_data import generateProductData
-from app.utils.response import get_file_response
+from app.utils.response import get_error_response, get_file_response
 
 
 router = APIRouter()
@@ -20,6 +20,12 @@ def create(
     request: Request,
     db_session: Session = Depends(get_session)
 ):
-    data = generateProductData(data_params, db_session)
-    outputPath = inventory_plot_create(data, product_params)
-    return get_file_response(filepath=outputPath, request=request, option=response_type)
+    try:
+        data = generateProductData(data_params, db_session)
+        if data.empty:
+            return JSONResponse(content=get_error_response(message="Error - No data exists for query"), status_code=404)
+        outputPath = inventory_plot_create(data, product_params)
+        return get_file_response(filepath=outputPath, request=request, option=response_type)
+    # exception handlers
+    except Exception as e:
+        return JSONResponse(content=get_error_response(message=str(e)), status_code=400)
